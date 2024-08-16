d5a5df1d4569a04f1f2eb20071e3fa120d40b90f
Jochen Klar <mail@jochenklar.de>
2024-08-16 13:37:59 +0200

    Refactor project tests for internal projects

--- rdmo/projects/tests/test_view_project.py
+++ rdmo/projects/tests/test_view_project.py
@@ -19,43 +19,47 @@ users = (
     ('author', 'author'),
     ('guest', 'guest'),
     ('user', 'user'),
-    ('site', 'site'),
-    ('anonymous', None),
     ('editor', 'editor'),
     ('reviewer', 'reviewer'),
     ('api', 'api'),
+    ('site', 'site'),
+    ('anonymous', None)
 )
 
 view_project_permission_map = {
-    'owner': [1, 2, 3, 4, 5, 10],
-    'manager': [1, 3, 5, 7],
-    'author': [1, 3, 5, 8],
-    'guest': [1, 3, 5, 9],
-    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    'owner': [1, 2, 3, 4, 5, 10, 12],
+    'manager': [1, 3, 5, 7, 12],
+    'author': [1, 3, 5, 8, 12],
+    'guest': [1, 3, 5, 9, 12],
+    'user': [12],
+    'editor': [12],
+    'reviewer': [12],
+    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 }
 
 change_project_permission_map = {
-    'owner': [1, 2, 3, 4, 5, 10],
+    'owner': [1, 2, 3, 4, 5, 10, 12],
     'manager': [1, 3, 5, 7],
-    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 }
 
 delete_project_permission_map = {
-    'owner': [1, 2, 3, 4, 5, 10],
-    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    'owner': [1, 2, 3, 4, 5, 10, 12],
+    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
 }
 
 export_project_permission_map = {
-    'owner': [1, 2, 3, 4, 5, 10],
+    'owner': [1, 2, 3, 4, 5, 10, 12],
     'manager': [1, 3, 5, 7],
-    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    'api': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    'site': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
 }
 
-projects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+projects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+projects_internal = [12]
 
 export_formats = ('rtf', 'odt', 'docx', 'html', 'markdown', 'tex', 'pdf')
 
@@ -78,8 +82,8 @@ def test_list(db, client, username, password):
         assertTemplateUsed(response, 'projects/projects.html')
 
         if username in ('site', 'api'):
-            assert projects == []
-            assert response.context['number_of_projects'] == len([])
+            assert sorted(set(map(int, projects))) == projects_internal
+            assert response.context['number_of_projects'] == 1
             assertContains(response, 'View all projects on')
         else:
             user_projects_map = view_project_permission_map.get(username, [])
@@ -231,7 +235,8 @@ def test_project_create_post(db, client, username, password):
     data = {
         'title': 'A new project',
         'description': 'Some description',
-        'catalog': catalog_id
+        'catalog': catalog_id,
+        'visibility': 'private'
     }
     response = client.post(url, data)
 
@@ -257,7 +262,8 @@ def test_project_create_post_restricted(db, client, settings):
     data = {
         'title': 'A new project',
         'description': 'Some description',
-        'catalog': catalog_id
+        'catalog': catalog_id,
+        'visibility': 'private'
     }
     response = client.post(url, data)
 
@@ -273,7 +279,8 @@ def test_project_create_post_forbidden(db, client, settings):
     data = {
         'title': 'A new project',
         'description': 'Some description',
-        'catalog': catalog_id
+        'catalog': catalog_id,
+        'visibility': 'private'
     }
     response = client.post(url, data)
 
@@ -290,7 +297,8 @@ def test_project_create_parent_post(db, client, username, password):
         'title': 'A new project',
         'description': 'Some description',
         'catalog': catalog_id,
-        'parent': parent_project_id
+        'parent': parent_project_id,
+        'visibility': 'private'
     }
     response = client.post(url, data)
 
@@ -332,7 +340,8 @@ def test_project_update_post(db, client, username, password, project_id):
     data = {
         'title': 'New title',
         'description': project.description,
-        'catalog': project.catalog.pk
+        'catalog': project.catalog.pk,
+        'visibility': 'private'
     }
     response = client.post(url, data)
 
@@ -359,7 +368,8 @@ def test_project_update_post_parent(db, client, username, password, project_id):
         'title': project.title,
         'description': project.description,
         'catalog': project.catalog.pk,
-        'parent': parent_project_id
+        'parent': parent_project_id,
+        'visibility': 'private'
     }
     response = client.post(url, data)
 
@@ -419,6 +429,47 @@ def test_project_update_information_post(db, client, username, password, project
             assert response.status_code == 302
 
         assert Project.objects.get(pk=project_id).title == project.title
+
+
+@pytest.mark.parametrize('username,password', users)
+@pytest.mark.parametrize('project_id', projects)
+def test_project_update_visibility_get(db, client, username, password, project_id):
+    client.login(username=username, password=password)
+
+    url = reverse('project_update_visibility', args=[project_id])
+    response = client.get(url)
+
+    if project_id in change_project_permission_map.get(username, []):
+        assert response.status_code == 200
+    else:
+        if password:
+            assert response.status_code == 403
+        else:
+            assert response.status_code == 302
+
+
+@pytest.mark.parametrize('username,password', users)
+@pytest.mark.parametrize('project_id', projects)
+def test_project_update_visibility_post(db, client, username, password, project_id):
+    client.login(username=username, password=password)
+    project = Project.objects.get(pk=project_id)
+
+    url = reverse('project_update_visibility', args=[project_id])
+    data = {
+        'visibility': 'internal'
+    }
+    response = client.post(url, data)
+
+    if project_id in change_project_permission_map.get(username, []):
+        assert response.status_code == 302
+        assert Project.objects.get(pk=project_id).visibility == 'internal'
+    else:
+        if password:
+            assert response.status_code == 403
+        else:
+            assert response.status_code == 302
+
+        assert Project.objects.get(pk=project_id).visibility == project.visibility
 
 
 @pytest.mark.parametrize('username,password', users)
