c3b453c56155f71917df6e6cb700206253887b60
Jochen Klar <mail@jochenklar.de>
2024-08-16 13:04:36 +0200

    Fix ProjectUpdateInformationForm

--- rdmo/projects/forms.py
+++ rdmo/projects/forms.py
@@ -80,7 +80,7 @@ class ProjectUpdateInformationForm(forms.ModelForm):
 
     class Meta:
         model = Project
-        fields = ('title', 'description', 'visibility')
+        fields = ('title', 'description')
 
 
 class ProjectUpdateVisibilityForm(forms.ModelForm):
